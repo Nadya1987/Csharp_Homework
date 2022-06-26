82bf2befa3a6ff115b625a58a40c0010f315c585
Nadya <you@example.com>
2022-06-26 22:05:54 +0300

    Task2 added

---
+++ Homework1_C#Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int num1, num2, num3;
+
+Console.Write("Введите первое число ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num1 > max) 
+{
+    max = num1;
+}
+if (num2 > max) 
+{
+    max = num2;
+}
+if (num3 > max)
+{
+    max = num3;
+}
+
+Console.Write("Максимальное число "+ max);
