9bd12567e175d7f51f351fca48f503ec7231ba6c
Nadya <you@example.com>
2022-06-26 21:40:09 +0300

    Task1

---
+++ Homework1_C#Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int numA, numB;
+
+Console.Write("Введите первое число ");
+numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA > numB)
+{
+        Console.Write("Большее число " + numA + ", а меньшее число " + numB );
+}
+else
+{
+    Console.Write("Большее число " + numB + ", а меньшее число " + numA );
+}
+
