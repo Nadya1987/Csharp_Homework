08c987a695e99f8e83037e35ffdb7dc9c2305a7b
Nadya <you@example.com>
2022-06-26 22:53:16 +0300

    Task3 added

---
+++ Homework1_C#Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int number;
+
+Console.Write("Привет! Введи любое число ");
+number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Это чётное число");
+}
+else
+{
+   Console.WriteLine("Это нечётное число ");
+}
