b0934a4378e87b1af508298f81816195790bab4b
Nadya <you@example.com>
2022-06-29 19:03:22 +0300

    Task4

---
+++ Homework1_C#Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int number;
+
+Console.Write("Привет! Введи любое число ");
+number = Convert.ToInt32(Console.ReadLine());
+
+while (number > 1)
+{
+    if(number % 2 == 0)
+    {
+        Console.Write("Вот такие чётные числа " + number);
+        number = number + 1;
+    }
+
+}
