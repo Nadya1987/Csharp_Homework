ab687d950d3d26870a4af1b838b52f644c182c85
Nadya <you@example.com>
2022-07-02 16:43:11 +0300

    Saved Task 1 and 2

---
+++ Homework3/Program.cs
@@ -0,0 +1,34 @@
+﻿
+// Task 1
+/*void Palindrom (int number)
+{
+    int dectys = number / 10000;
+    int tys = number / 1000;
+    int ed = number % 10;
+
+    if(dectys == ed || tys == dectys * 10) Console.WriteLine("Это палиндром!");
+        else Console.WriteLine("Не палиндром");
+}
+
+Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Palindrom(num);*/
+
+//Task2
+
+void Cube(int num) 
+{ 
+     int current = 1; 
+     while (current <= num) 
+     { 
+        int cube = current * current * current; 
+        Console.Write(cube + " "); 
+        current++; 
+     } 
+} 
+
+Console.WriteLine("Введите число: "); 
+int number = Convert.ToInt32(Console.ReadLine()); 
+Cube(number); 
+
