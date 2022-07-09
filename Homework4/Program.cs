20acc5cd63e639f065544e3960ced4732207e791
Nadya <you@example.com>
2022-07-09 22:36:10 +0300

    Homework 4

---
+++ Homework4/Program.cs
@@ -0,0 +1,33 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт 
+//сумму цифр в числе.
+/*
+int[] Sum(int size)
+  {
+    int sum = 0;
+    int[] newArray = new int[size];
+    
+    for(int i = 0; i < size; i++)
+    {
+        sum = sum + newArray[i];
+    }
+    return newArray;
+  }
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Сумма цифр в числе: " + Sum);*/
+
+//Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+
+int[] Massiv(int size)
+{
+  int[]newArray = new int[size];
+  
+  return newArray;
+}
+
+Console.Write("Задайте массив: ");
+int massiv = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("[" + massiv + "]");
